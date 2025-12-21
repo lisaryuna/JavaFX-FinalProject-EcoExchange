ccafdfa1bf2d20cfef6cd006118318bbfde7522e
Lisa <fa037024@gmail.com>
2025-12-21 13:41:58 +0800

    update bonus (silver, gold)

--- EcoExchange/src/main/java/com/example/ecoexchange/service/TransactionService.java
+++ EcoExchange/src/main/java/com/example/ecoexchange/service/TransactionService.java
@@ -3,20 +3,34 @@ package com.example.ecoexchange.service;
 import com.example.ecoexchange.dao.UserDAO;
 import com.example.ecoexchange.dao.WithdrawalDAO;
 import com.example.ecoexchange.model.Customer;
+import com.example.ecoexchange.model.MembershipLevel;
 import com.example.ecoexchange.model.WasteCategory;
 
 public class TransactionService {
     private UserDAO userDAO = new UserDAO();
     private WithdrawalDAO withdrawalDAO = new WithdrawalDAO();
 
+    private static final double LIMIT_SILVER = 20.0;
+    private static final double LIMIT_GOLD = 50.0;
+
     public void processDeposit(Customer customer, WasteCategory wasteCategory, double weight) {
-        double baseTotal = wasteCategory.getBasePrice() * weight;
-
-        double bonus = customer.getPriceBonus();
-
-        double totalEarned = baseTotal + bonus;
+        double pricePerKg = wasteCategory.getBasePrice() + customer.getPriceBonus();
+        double totalEarned = pricePerKg * weight;
 
         customer.addDeposit(weight, totalEarned);
+
+        double currentTotalWeight = customer.getTotalWeightHistory();
+        MembershipLevel oldLevel = customer.getMembershipLevel();
+
+        if (currentTotalWeight >= LIMIT_GOLD) {
+            customer.setMembershipLevel(MembershipLevel.GOLD);
+        } else if (currentTotalWeight >= LIMIT_SILVER) {
+            customer.setMembershipLevel(MembershipLevel.SILVER);
+        }
+
+        if (customer.getMembershipLevel() != oldLevel) {
+            System.out.println("LEVEL UP!");
+        }
 
         userDAO.updateCustomerStats(customer);
 
